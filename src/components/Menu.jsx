0acc30ab6b2462799e718cd6644e3c976ba6dd22
Aroniari <meriton12345@gmail.com>
2024-11-21 20:39:15 +0100

    I add the images

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 
 const menuItems = [
-  { name: 'Burger', description: 'A delicious burger with all the fixings.', price: 79.99 },
-  { name: 'Pizza', description: 'Classic pizza with your favorite toppings.', price: 99.99 },
-  { name: 'Fries', description: 'Crispy golden fries served hot and fresh.', price: 59.99 },
+  {
+    name: 'Burger',
+    description: 'A delicious burger with all the fixings.',
+    price: 79.99,
+    image: '/images/burger.jpg',
+  },
+  {
+    name: 'Pizza',
+    description: 'Classic pizza with your favorite toppings.',
+    price: 99.99,
+    image: '/images/pizza.jpg',
+  },
+  {
+    name: 'Fries',
+    description: 'Crispy golden fries served hot and fresh.',
+    price: 59.99,
+    image: '/images/fries.jpg',
+  },
 ];
 
 const Menu = ({ addToOrder }) => {
@@ -14,12 +29,20 @@ const Menu = ({ addToOrder }) => {
         {menuItems.map((item) => (
           <div className="col-md-4" key={item.name}>
             <div className="card mb-4">
-              <img src={`/images/${item.name.toLowerCase()}.jpg`} className="card-img-top" alt={item.name} />
+              {/* Display the image */}
+              <img
+                src={item.image}
+                className="card-img-top"
+                alt={item.name}
+                style={{ height: '200px', objectFit: 'cover' }}
+              />
               <div className="card-body">
                 <h5 className="card-title">{item.name}</h5>
                 <p className="card-text">{item.description}</p>
                 <p className="card-text">SEK {item.price.toFixed(2)}</p>
-                <button className="btn btn-primary" onClick={() => addToOrder(item.name)}>Add to Order</button>
+                <button className="btn btn-primary" onClick={() => addToOrder(item.name)}>
+                  Add to Order
+                </button>
               </div>
             </div>
           </div>
